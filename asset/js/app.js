f0de6d1e3f118dae7413f8c3056dc20cc13343da
D-Remi <delfosseremi@hotmail.com>
2020-10-15 14:52:59 +0200

    first commit

--- asset/js/app.js
+++ asset/js/app.js
@@ -1,37 +1,48 @@
-function apiMovie(){
-
-    fetch('https://api.themoviedb.org/3/movie/popular?api_key=0986c498f06bb84d2afe90e6adecf322&language=en-US&page=1')
-    .then(function(resp) { return resp.json() })
-    .then(function(data) {
-        console.log(data);
-        getMovie(data)
-    })
-    .catch(function(err) {
-        console.log(err)
-    });
-}
-
-function getMovie(w) {
-    
-    let ul = document.getElementById('movie');
-    const result = w.results;
-
-
-    
-    for(let i = 0; i < 15; i++){
-        
-        var html = `
-        <a href='movie.html'><img src="//image.tmdb.org/t/p/w220_and_h330_face/`+ result[i].backdrop_path+`"></a>
-        <h3>`+ result[i].title +`</h3><br>
-        
-        `
-        var li= document.createElement("li");
-        ul.append(li);
-        li.innerHTML = html;
+function getMovies() {
+    query = document.getElementById('val').value;
+    if(query == ''){
+        fetch('https://api.themoviedb.org/3/movie/popular?api_key=0986c498f06bb84d2afe90e6adecf322&language=en-US&page=1')
+        .then(function(resp) { return resp.json() })
+        .then(function(data) {
+            console.log(data);
+            showMovies(data)
+        })
+        .catch(function(err) {
+            console.log(err)
+        });
+    }else{
+        fetch('https://api.themoviedb.org/3/search/movie?api_key=0986c498f06bb84d2afe90e6adecf322&language=en-US&query='+ query +'&page=1&include_adult=false')
+        .then(function(resp) { return resp.json() })
+        .then(function(data) {
+            // console.log(data);
+            showMovies(data);
+        })
+        .catch(function(err) {
+            console.log(err)
+        });
     }
 }
 
+function showMovies(m) {
+    
+    let ul = document.getElementById('movie');
+    const result = m.results;
+    //delete the LI of UL
+    
+    for(let i = 0; i < 15; i++){
+        let html = `
+        <a href='movie.html'><img src="//image.tmdb.org/t/p/w220_and_h330_face/`+ result[i].backdrop_path+`"></a>
+        <h3>`+ result[i].title +`</h3><br>
+        <span>`+ result[i].release_date +`</span>
+        `
+        let li= document.createElement("li");
+        ul.append(li);
+        li.innerHTML = html;
+    }
+    
+    
+}
+
 window.onload = function(){
-    apiMovie();
-    getMovie()
+    getMovies();
 }
